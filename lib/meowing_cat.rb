3ec50404ee3be1f0bbafeb8113742be05bb030ca
Charlie Oster <oster.charlie@yahoo.com>
2019-10-12 20:35:55 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,26 +1,3 @@
-class Cat 
-  
-  attr_accessor :name
-  
-  def initialize(name)
-    @name = name
-  end
-end
 
-class Cat
- 
-  attr_accessor :meow 
-  
-  def initialize(meow)
-    @meow = meow 
-  end
-end
-
-
-
-
-maru.meow = "Meow" 
-maru.meow class Food
- 
  
  
