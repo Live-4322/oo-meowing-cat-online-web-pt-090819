d09bc74ee84a13449fd18b556aa3bdee5e2b377e
Charlie Oster <oster.charlie@yahoo.com>
2019-10-12 20:34:19 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,26 @@
-## code your solution here. 
+class Cat 
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cat
+ 
+  attr_accessor :meow 
+  
+  def initialize(meow)
+    @meow = meow 
+  end
+end
+
+
+
+
+maru.meow = "Meow" 
+maru.meow class Food
+ 
+ 
+ 
